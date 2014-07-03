a77c304fdb7f960cb8ea74093caa0449bd33a491
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-07-03 18:11:10 -0400

    start trivia game

--- modules/trivia.py
+++ modules/trivia.py
@@ -1,5 +1,23 @@
+class Trivia:
+	def __init__(self, circa, channel, host):
+		self.circa = circa
+		self.users = list(circa.channels[channel].users.keys())
+		self.scores = {user: 0 for user in self.users}
+
+		self.host = host
+
 class TriviaModule:
 	def __init__(self, circa):
 		self.circa = circa
+		self.games = {}
+
+		self.listeners = {
+			"cmd.trivia": [self.start]
+		}
+
+	def start(self, fr, to, params):
+		if to in self.circa.channels and len(params):
+			host = params[0]
+			self.games[to] = game = Trivia(self.circa, to, host)
 
 module = TriviaModule

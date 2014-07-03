27f8ae059a67745da6a9a0af74ceb308bf80a776
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-07-03 18:28:08 -0400

    add start/end

--- modules/trivia.py
+++ modules/trivia.py
@@ -12,12 +12,30 @@ class TriviaModule:
 		self.games = {}
 
 		self.listeners = {
-			"cmd.trivia": [self.start]
+			"cmd.trivia": [self.start],
+			"cmd.endtrivia": [self.end],
 		}
 
 	def start(self, fr, to, params):
-		if to in self.circa.channels and len(params):
-			host = params[0]
-			self.games[to] = game = Trivia(self.circa, to, host)
+		if len(params) == 0:
+			return
+		host = params[0]
+		if to in self.games:
+			self.circa.say(to, "trivia already running in {0}".format(to))
+		elif to in self.circa.channels:
+			if host in self.circa.channels[to].users:
+				self.games[to] = game = Trivia(self.circa, to, host)
+				self.circa.say(to, "started trivia in {0} - your host is {1}".format(to, host))
+			else:
+				self.circa.say(to, "no user {0} in {1}".format(host, to))
+		else:
+			self.circa.say(to, "cannot start trivia here")
+
+	def end(self, fr, to, params):
+		if to in self.games:
+			game = self.games[to].host
+			...
+		else:
+			self.circa.say("no trivia running in {0}".format(to))
 
 module = TriviaModule

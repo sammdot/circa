0ce089d8cabb19a2b1af162d7902a0e800a9baac
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-07-03 23:55:48 -0400

    add url module, add @info to infobot module

--- modules/infobot.py
+++ modules/infobot.py
@@ -7,7 +7,7 @@ class InfobotModule:
 		}
 
 	def version(self, fr, to, text, msg):
-		if text.lower() == "!info circa":
-			self.circa.say(to, "circa/0.1 (Python/3.4.0 sdirc/1.0) by sammi http://github.com/sammdot/circa")
+		if text.lower() == "!info circa" or text.lower() == "@info circa":
+			self.circa.say(to, "circa (Python/3.4) by sammi github:sammdot/circa")
 
 module = InfobotModule

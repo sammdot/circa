2c42e644c17ba2c26ccb7fa59a593440fa9de9c8
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-07-03 20:27:32 -0400

    remove 404

--- modules/infobot.py
+++ modules/infobot.py
@@ -8,6 +8,6 @@ class InfobotModule:
 
 	def version(self, fr, to, text, msg):
 		if text.lower() == "!info circa":
-			self.circa.say(to, "circa/0.1 (Python/3.4.0 sdirc/1.0) by sammi http://github.com/sammdot/circa [\x02404\x02]")
+			self.circa.say(to, "circa/0.1 (Python/3.4.0 sdirc/1.0) by sammi http://github.com/sammdot/circa")
 
 module = InfobotModule

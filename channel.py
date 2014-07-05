a18efce78948467e71f33b85c4a21530978d3b43
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-07-05 18:32:40 -0400

    use nick dict class instead of native dict

--- channel.py
+++ channel.py
@@ -1,9 +1,21 @@
 from util import nickcmp, nicklower
 
+class NickDict(dict):
+	def __setitem__(self, nick, value):
+		dict.__setitem__(self, nicklower(nick), value)
+
+	def __getitem__(self, nick):
+		return dict.__getitem__(self, nicklower(nick))
+
+	def __contains__(self, nick):
+		return dict.__contains__(self, nicklower(nick))
+
 class Channel:
 	def __init__(self, name):
 		self.name = name
-		self.users = {}
+		self.users = NickDict()
+		self.topic = None
+		self.mode = set()
 
 	def __contains__(self, nick):
-		return nicklower(nick) in self.users.keys()
+		return nicklower(nick) in self.users

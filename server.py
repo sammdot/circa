50b3a4fb1d0e2c1aadd3b00d0e95bda1e7b80815
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-07-19 15:48:15 -0400

    add server object

---
+++ server.py
@@ -0,0 +1,23 @@
+class Server:
+	def __init__(self, host, port):
+		self.host = host
+		self.port = port
+
+		self.motd = ""
+
+		self.usermods = set()
+		self.kicklength = 0
+		self.maxlist = {}
+		self.maxtargets = {}
+		self.modes = 3
+		self.nicklength = 9
+		self.topiclength = 0
+
+		self.idlength = {}
+		self.chlength = 200
+		self.chlimit = {}
+		self.chmods = {x: set() for x in "abcd"}
+		self.types = set()
+
+		self.prefix_mode = {}
+		self.mode_prefix = {}

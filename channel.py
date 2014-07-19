6849ae9f0156c9af615a41ced818cd74d39ba863
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-07-19 17:05:52 -0400

    add channel and nick dictionary

---
+++ channel.py
@@ -0,0 +1,25 @@
+from util import nickeq, nicklower
+
+class NickDict(dict):
+	def __setitem__(self, nick, value):
+		dict.__setitem__(self, nicklower(nick), value)
+	
+	def __getitem__(self, nick):
+		return dict.__getitem__(self, nicklower(nick))
+	
+	def __contains__(self, nick):
+		return dict.__contains__(self, nicklower(nick))
+
+class Channel:
+	def __init__(self, name):
+		self.name = name
+		self.users = NickDict()
+		self.topic = None
+		self.mode = set()
+	
+	def __repr__(self):
+		return "Channel({0})".format(self.name)
+	
+	def __contains__(self, nick):
+		return nick in self.users
+

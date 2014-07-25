f19e69d825728cbd53370a6aa3a1ae5468b46b39
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-07-25 01:31:14 -0400

    add required settings check

--- circa.py
+++ circa.py
@@ -6,6 +6,13 @@ class Circa(client.Client):
 		conf["autoconn"] = False
 		logging.basicConfig(filename=conf.get("log", "circa.log"), level=logging.INFO,
 			style="%", format="%(asctime)s %(levelname)s %(message)s")
+
+		for setting in "server nick username realname".split():
+			if setting not in conf:
+				logging.error("Required setting %s not present", setting)
+				logging.info("See %s for details", conf.get("log", "circa.log"))
+				exit(1)
+
 		if "log" in conf:
 			logging.basicConfig(filename=conf["log"])
 		client.Client.__init__(self, **conf)

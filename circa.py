ebab02fc28fd5a4739ac4031b84177d36257eb78
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-07-25 13:05:52 -0400

    add register and invite handlers

--- circa.py
+++ circa.py
@@ -4,8 +4,9 @@ import client
 class Circa(client.Client):
 	def __init__(self, conf):
 		conf["autoconn"] = False
-		logging.basicConfig(filename=conf.get("log", "circa.log"), level=logging.INFO,
-			style="%", format="%(asctime)s %(levelname)s %(message)s")
+		logging.basicConfig(filename=conf.get("log", "circa.log"),
+			level=logging.DEBUG, style="%",
+			format="%(asctime)s %(levelname)s %(message)s")
 
 		for setting in "server nick username realname".split():
 			if setting not in conf:
@@ -14,3 +15,15 @@ class Circa(client.Client):
 				exit(1)
 
 		client.Client.__init__(self, **conf)
+
+		logging.info("Registering callbacks")
+		self.add_listener("registered", self.registered)
+		self.add_listener("invite", self.join)
+
+	def registered(self, nick):
+		self.send("UMODE2", "+B")
+		if "password" in self.conf:
+			self.send("PRIVMSG", "NickServ", "IDENTIFY {0}".format(
+					str(self.conf["password"])))
+		for chan in self.conf["channels"]:
+			self.join("#" + chan)

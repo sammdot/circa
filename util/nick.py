ae9a71b0936e1d9e36ab44773cfd683f33a79fd9
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-07-27 13:40:00 -0400

    use string.translate for nick lowercase

--- util/nick.py
+++ util/nick.py
@@ -1,6 +1,7 @@
+import string
+
 def nicklower(nick):
-	return nick.lower().replace("[", "{").replace("]", "}") \
-		.replace("|", "\\").replace("^", "~")
+	return string.translate(nick.lower(), string.maketrans("[]\\~", "{}|^")
 
 def nickeq(nick1, nick2):
 	return nicklower(nick1) == nicklower(nick2)

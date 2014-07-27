35e43c369dfa755b938d1a7ccd12ff69458d008c
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-07-27 13:44:51 -0400

    more translation table outside

--- util/nick.py
+++ util/nick.py
@@ -1,7 +1,9 @@
 import string
 
+trans = string.maketrans("[]\\~", "{}|^")
+
 def nicklower(nick):
-	return string.translate(nick.lower(), string.maketrans("[]\\~", "{}|^")
+	return string.translate(nick.lower(), trans)
 
 def nickeq(nick1, nick2):
 	return nicklower(nick1) == nicklower(nick2)

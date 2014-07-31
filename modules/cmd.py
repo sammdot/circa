91920b0cf9f54b13cc9e4bc1bc1534d63f7b2582
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-07-30 21:01:20 -0400

    fix cmd module

--- modules/cmd.py
+++ modules/cmd.py
@@ -2,17 +2,15 @@ class CommandModule:
 	def __init__(self, circa):
 		self.circa = circa
 
-	def onload(self):
-		self.circa.add_listener("message", self.handle_cmd)
+		self.events = {
+			"message": [self.handle_cmd]
+		}
 
-	def onunload(self):
-		self.circa.remove_listener("message", self.handle_cmd)
-
-	def handle_cmd(self, fr, to, text):
+	def handle_cmd(self, fr, to, text, m):
 		if text.startswith(self.circa.conf["prefix"]):
 			cmd = text.split(" ")[0]
 			cmdname = "$" if cmd == self.circa.conf["prefix"] else cmd[1:]
 			self.circa.emit("cmd." + cmdname, fr, fr if to == self.circa.nick else to,
-				" ".join(text.split(" ")[1:]))
+				" ".join(text.split(" ")[1:]), m)
 
 module = CommandModule

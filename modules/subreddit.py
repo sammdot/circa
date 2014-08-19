12995be9490bde60c92e6f962b748832c083fe45
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-08-19 15:20:24 -0400

    use API and HTTP HEAD instead

--- modules/subreddit.py
+++ modules/subreddit.py
@@ -1,5 +1,6 @@
 import re
 import urllib.request as req
+import urllib.error   as err
 
 class SubredditModule:
 	subre = re.compile(r"^(?:.* )?/r/([A-Za-z0-9][A-Za-z0-9_]{2,20})")
@@ -12,11 +13,12 @@ class SubredditModule:
 
 	def findsub(self, fr, to, msg, m):
 		for sub in self.subre.findall(msg):
-			url = "http://www.reddit.com/r/" + sub
 			try:
-				req.urlopen(url)
-				self.circa.say(to, url)
-			except:
+				r = req.Request("http://api.reddit.com/r/" + sub + ".json")
+				r.get_method = lambda: "HEAD"
+				req.urlopen(r)
+				self.circa.say(to, "http://www.reddit.com/r/" + sub)
+			except err.HTTPError as e:
 				pass
 
 module = SubredditModule

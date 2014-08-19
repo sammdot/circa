86456d399a02255ed30019c778998988c842a2f7
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-08-19 14:41:48 -0400

    basic subreddit linking

---
+++ modules/subreddit.py
@@ -0,0 +1,17 @@
+import http.client as client
+import re
+
+class SubredditModule:
+	subre = re.compile(r"/r/[A-Za-z0-9][A-Za-z0-9_]{2,20}")
+
+	def __init__(self, circa):
+		self.circa = circa
+		self.events = {
+			"message": [self.findsub]
+		}
+
+	def findsub(self, fr, to, msg, m):
+		for sub in self.subre.findall(msg):
+			self.circa.say(to, "http://www.reddit.com" + sub)
+
+module = SubredditModule

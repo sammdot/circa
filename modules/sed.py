93587d3edc8d6b98091fe32d349a65902e1f9346
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-08-23 19:18:40 -0400

    remove unescape

--- modules/sed.py
+++ modules/sed.py
@@ -1,6 +1,8 @@
 import re
 import string
 
+from util.esc import unescape
+
 """
 The 'tr' implementation was based on github:ikegami-yukino/python-tr.
 """
@@ -43,36 +45,6 @@ def squeeze(lst, src):
 
 def tr(frm, to, src):
 	return src.translate(str.maketrans(frm, to))
-
-def unescape(text):
-	"""Unescape function based on http://stackoverflow.com/a/15528611."""
-	regex = re.compile(b'\\\\(\\\\|[0-7]{1,3}|x.[0-9a-f]?|[\'"abfnrt]|.|$)')
-	def replace(m):
-		b = m.group(1)
-		if len(b) == 0:
-			raise ValueError("Invalid character escape: '\\'.")
-		i = b[0]
-		if i == 120:
-			v = int(b[1:], 16)
-		elif 48 <= i <= 55:
-			v = int(b, 8)
-		elif i == 34: return b'"'
-		elif i == 39: return b"'"
-		elif i == 92: return b'\\'
-		elif i == 97: return b'\a'
-		elif i == 98: return b'\b'
-		elif i == 102: return b'\f'
-		elif i == 110: return b'\n'
-		elif i == 114: return b'\r'
-		elif i == 116: return b'\t'
-		else:
-			s = b.decode('ascii')
-			raise UnicodeDecodeError(
-				'stringescape', text, m.start(), m.end(), "Invalid escape: %r" % s
-			)
-		return bytes((v, ))
-	result = regex.sub(replace, bytes(text, "utf-8"))
-	return str(result, "utf-8")
 
 class SedModule:
 	subre = re.compile(r"^(?:(\S+)[:,]\s)?(?:s|(.+?)/s)/((?:\\/|[^/])+)\/((?:\\/|[^/])*?)/([gixs]{0,4})?(?: .*)?$")

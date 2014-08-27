a65354228cfff3acf0260a2e7ebf3f5a991156d8
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-08-27 18:41:45 -0400

    list commands in module

--- modules/help.py
+++ modules/help.py
@@ -5,13 +5,19 @@ class HelpModule:
 			"cmd.help": [self.help]
 		}
 		self.docs = {
-			"help": "help <module>[.<command>] → show usage info for a command, or list all commands in a module"
+			"help": "help [<module>[.<command>]] → show usage info for a command, or list all commands in a module, or list all modules"
 		}
 
 	def help(self, fr, to, msg, m):
 		msg = msg.split(" ", 1)[0]
 		if msg:
-			...
+			c = msg.split(".", 1)
+			module = self.circa.modules[c[0]]
+			if len(c) == 1: # module
+				commands = sorted(module.docs.keys())
+				self.circa.say(to, "Available commands: " + " ".join(commands))
+			else: # command
+				pass
 		else:
 			modules = sorted([i for i in self.circa.modules.keys() if \
 				hasattr(self.circa.modules[i], "docs")])

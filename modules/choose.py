c33f2e2e7478becf9ceddbe307b36289dccca912
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-08-27 17:53:17 -0400

    roll command

--- modules/choose.py
+++ modules/choose.py
@@ -4,9 +4,23 @@ class ChooseModule:
 	def __init__(self, circa):
 		self.circa = circa
 		self.events = {
+			"cmd.roll": [self.roll],
 			"message": [self.dotchoose],
 			"cmd.choose": [self.choose]
 		}
+
+	def roll(self, fr, to, msg, m):
+		a, b = 1, 6
+		msg = msg.strip().split(" ", 1)
+		if msg:
+			l, r = msg[0].split("d")
+			try:
+				a = int(l)
+				b = int(r)
+			except ValueError:
+				pass
+		values = [str(random.randint(1, b)) for i in range(a)]
+		self.circa.say(to, fr + ": " + ", ".join(values))
 
 	def choose(self, fr, to, msg, m):
 		opts = [opt.strip() for opt in msg.split(",")]

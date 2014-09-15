d226ceb3b21381792e285eeae379c4388dc7b274
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-09-15 14:18:12 -0400

    print diff-ed output to channel

--- modules/romaji.py
+++ modules/romaji.py
@@ -13,8 +13,14 @@ class RomajiModule:
 
 	def trans(self, fr, to, msg, m):
 		text = shlex.quote(msg)
-		out = subprocess.check_output("echo {0} | iconv -f utf8 -t eucjp | kakasi -i euc -w | " \
+		out = subprocess.check_output("echo -n {0} | iconv -f utf8 -t eucjp | kakasi -i euc -w | " \
 			"kakasi -i euc -Ha -Ja -Ka -Ea -ka".format(text), shell=True).decode("utf-8")
-		self.circa.say(to, diff(text, out))
+		# TODO: process kakasi output
+		if msg != out:
+			d = diff(msg, out)
+			if msg.startswith("\x01ACTION "):
+				self.circa.say(to, "\x02* {0}\x02 ".format(fr) + d[len("\x01ACTION "):-1])
+			else:
+				self.circa.say(to, "<{0}> ".format(fr) + d)
 
 module = RomajiModule

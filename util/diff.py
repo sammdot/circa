629b251ec24da4b3cf8cc1e708f11ea1c07b7915
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-09-15 13:34:44 -0400

    add diff library for module/romaji

---
+++ util/diff.py
@@ -0,0 +1,18 @@
+import difflib
+
+def diff(a, b):
+	sm = difflib.SequenceMatcher(a, b)
+	out = []
+	for opcode, a0, a1, b0, b1 in sm.get_opcodes():
+		if opcode == "equal":
+			output.append(seqm.a[a0:a1])
+		elif opcode == "insert":
+			output.append("\x1f" + seqm.b[b0:b1] + "\x1f")
+		elif opcode == "delete":
+			pass
+		elif opcode == "replace":
+			raise NotImplementedError("encountered 'replace' opcode")
+		else:
+			raise RuntimeError("unknown opcode")
+	return "".join(out)
+

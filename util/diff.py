8ffbcc4ef80714f2396cf0700e7d78333157502d
Sammi De Guzman <sammi@sammdot.ca>
2016-05-24 13:43:33 -0400

    use inverted mode instead of underline for diff output

--- util/diff.py
+++ util/diff.py
@@ -7,11 +7,11 @@ def diff(a, b):
 		if opcode == "equal":
 			out.append(sm.a[a0:a1])
 		elif opcode == "insert":
-			out.append("\x1f" + sm.b[b0:b1] + "\x1f")
+			out.append("\x16" + sm.b[b0:b1] + "\x16")
 		elif opcode == "delete":
 			pass
 		elif opcode == "replace":
-			out.append("\x1f" + sm.b[b0:b1] + "\x1f")
+			out.append("\x16" + sm.b[b0:b1] + "\x16")
 		else:
 			raise RuntimeError("unknown opcode")
 	return "".join(out)
